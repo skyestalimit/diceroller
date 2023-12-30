06b91484f110bc02ac2eb02a498dcabae3a80511
SkyEstaLimit <skyestalimit@gmail.com>
2023-12-30 15:53:03 -0500

    Code cleanup

--- diceRoll.go
+++ diceRoll.go
@@ -13,27 +13,23 @@ type DiceRoll struct {
 	Modifier 	int
 }
 
-// func NewDiceRoll(diceAmmount, diceSize, modifier) *DiceRoll{
-// 	return &diceRoll := DiceRoll {diceAmmount, diceSize, modifier}
-// }
-
-// Basic single dice roll
-
+// Basic single DiceRoll
 func PerformSingleRoll(diceRoll DiceRoll) int {
-
-	fmt.Println("Rolling DiceRoll: ", diceRoll)
+	fmt.Println("Rolling for", printoutDiceRoll(diceRoll))
 	rollSum := 0
+	rollStr := ""
 	for i := 0; i < diceRoll.DiceAmmount; i++ {
 		diceGen := getFreshRandomGenerator()
 		diceRollResult := diceGen.Intn(diceRoll.DiceSize) + 1
-		fmt.Println("Gen results: ", diceRollResult)
+		rollStr += fmt.Sprintf("%d ", diceRollResult)
 		rollSum += diceRollResult
 	}
-	fmt.Println("Roll sum: ", rollSum)
-	fmt.Println("Roll sum with mod: ", rollSum + diceRoll.Modifier)
+	fmt.Println("Dice rolls:", rollStr)
+	fmt.Println(fmt.Sprintf("Roll sum with mod for %s: %d", printoutDiceRoll(diceRoll), rollSum + diceRoll.Modifier))
 	return rollSum + diceRoll.Modifier
 }
 
+// Perform an array of DiceRoll
 func PerformRolls(diceRolls []DiceRoll) int {
 	rollsSum := 0
 	for i := 0; i < len(diceRolls); i++ {
@@ -44,4 +40,15 @@ func PerformRolls(diceRolls []DiceRoll) int {
 
  func getFreshRandomGenerator() *rand.Rand {
  	return rand.New(rand.NewSource(time.Now().UnixNano()))
+ }
+
+ func printoutDiceRoll(diceRoll DiceRoll) string {
+	strDiceRoll := fmt.Sprintf("%dd%d", diceRoll.DiceAmmount, diceRoll.DiceSize)
+	if diceRoll.Modifier != 0 {
+		if diceRoll.Modifier > 0 {
+			strDiceRoll += "+"
+		} 	
+		strDiceRoll += fmt.Sprintf("%d", diceRoll.Modifier)	
+	}
+	return strDiceRoll
  }
