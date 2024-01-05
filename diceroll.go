b2266a838c9e01f16411b7c9a849cd169cb921c3
SkyEstaLimit <skyestalimit@gmail.com>
2024-01-04 19:40:07 -0500

    comments and optimizations

--- diceroll.go
+++ diceroll.go
@@ -7,23 +7,17 @@ import (
 	"time"
 )
 
-// Dice Roll structure
+// DiceRoll structure
 type DiceRoll struct {
 	DiceAmmount int
 	DiceSize    int
 	Modifier    int
 }
 
-// Dice Roll Result structure
-type DiceRollResult struct {
-	DiceRollStr string
-	Dice        []int
-	Sum         int
-}
-
+// Max allowed value for DiceRoll definition.
 const MaxDiceRollValue int = 99999
 
-// DiceRoll constructor
+// DiceRoll constructor. Validates DiceRoll values.
 func NewDiceRoll(ammount int, size int, modifier int) (DiceRoll, error) {
 	diceRoll := DiceRoll{ammount, size, modifier}
 	if diceErr := validateDiceRoll(diceRoll); diceErr == nil {
@@ -33,36 +27,28 @@ func NewDiceRoll(ammount int, size int, modifier int) (DiceRoll, error) {
 	}
 }
 
-// DiceRollResult constructor
-func NewDiceRollResult(diceRollStr string) DiceRollResult {
-	return DiceRollResult{diceRollStr, []int{}, 0}
-}
-
-// Perform an array of DiceRoll
-func PerformRolls(diceRolls []DiceRoll) (map[*DiceRoll]*DiceRollResult, []error) {
-	diceRollMap := map[*DiceRoll]*DiceRollResult{}
-	diceErrs := make([]error, 0)
+// Perform an array of DiceRoll. Returns a DiceRollResult array for valid DiceRolls and an error array for invalid ones.
+func PerformRolls(diceRolls []DiceRoll) (results []DiceRollResult, diceErrs []error) {
 	for i := range diceRolls {
 		if result, diceErr := diceRolls[i].PerformRoll(); diceErr == nil {
-			diceRollMap[&diceRolls[i]] = result
+			results = append(results, *result)
 		} else {
 			diceErrs = append(diceErrs, diceErr)
 		}
 	}
-	return diceRollMap, diceErrs
+	return results, diceErrs
 }
 
-// Performs a DiceRoll
-// Returns DiceRollResult
+// Validates and performs a DiceRoll. Returns DiceRollResult if valid, an error if invalid.
 func (diceRoll DiceRoll) PerformRoll() (*DiceRollResult, error) {
 	// Validate DiceRoll values
 	if diceErr := validateDiceRoll(diceRoll); diceErr != nil {
+		// Invalid DiceRoll, return error
 		return nil, diceErr
 	}
-	diceRollResult := new(DiceRollResult)
 
-	// Generate rolls DiceAmmount times for DiceSize dices
-	fmt.Println("Rolling for " + diceRoll.String())
+	// Generate rolls DiceAmmount times of DiceSize dices
+	diceRollResult := NewDiceRollResult(diceRoll.String())
 	for i := 0; i < diceRoll.DiceAmmount; i++ {
 		diceGen := getFreshRandomGenerator()
 		diceRollResult.Dice = append(diceRollResult.Dice, diceGen.Intn(diceRoll.DiceSize)+1)
@@ -71,18 +57,22 @@ func (diceRoll DiceRoll) PerformRoll() (*DiceRollResult, error) {
 
 	// Apply modifier
 	diceRollResult.Sum += diceRoll.Modifier
+
 	// Minimum roll result if always 1
 	if diceRollResult.Sum <= 0 {
 		diceRollResult.Sum = 1
 	}
 
-	fmt.Println(diceRollResult.String())
+	// Valid DiceRoll, return DiceRollResult
 	return diceRollResult, nil
 }
 
-// Human readable DiceRoll String
+// Human readable DiceRoll string
 func (diceRoll DiceRoll) String() string {
+	// XdX format
 	strDiceRoll := fmt.Sprintf("%dd%d", diceRoll.DiceAmmount, diceRoll.DiceSize)
+
+	// Add modifier when necessary
 	if diceRoll.Modifier != 0 {
 		if diceRoll.Modifier > 0 {
 			strDiceRoll += "+"
@@ -92,6 +82,7 @@ func (diceRoll DiceRoll) String() string {
 	return strDiceRoll
 }
 
+// Validates DiceRoll values. Returns nil if valid, error if invalid.
 func validateDiceRoll(diceRoll DiceRoll) error {
 	if diceErr := validateDiceAmmout(diceRoll.DiceAmmount); diceErr != nil {
 		return fmt.Errorf("dice roll %s, %s", diceRoll.String(), diceErr.Error())
@@ -105,6 +96,7 @@ func validateDiceRoll(diceRoll DiceRoll) error {
 	return nil
 }
 
+// Validates ammount value for DiceRoll. Returns nil if valid, error if invalid.
 func validateDiceAmmout(ammount int) error {
 	if ammount > MaxDiceRollValue || ammount <= 0 {
 		return fmt.Errorf("invalid dice ammout: %d", ammount)
@@ -112,6 +104,7 @@ func validateDiceAmmout(ammount int) error {
 	return nil
 }
 
+// Validates size value for DiceRoll. Returns nil if valid, error if invalid.
 func validateDiceSize(size int) error {
 	if size > MaxDiceRollValue || size <= 1 {
 		return fmt.Errorf("invalid dice size: %d", size)
@@ -119,6 +112,7 @@ func validateDiceSize(size int) error {
 	return nil
 }
 
+// Validates modifier value for DiceRoll. Returns nil if valid, error if invalid.
 func validateDiceModifier(modifier int) error {
 	if int(math.Abs(float64(modifier))) > MaxDiceRollValue {
 		return fmt.Errorf("invalid dice modifier: %d", modifier)
@@ -126,12 +120,7 @@ func validateDiceModifier(modifier int) error {
 	return nil
 }
 
-// Human readable DiceRollResult String
-func (result DiceRollResult) String() string {
-	return fmt.Sprintf("Dice rolls: %s Sum: %d", fmt.Sprint(result.Dice), result.Sum)
-}
-
-// Seeds a fresh random generator
+// Seeds a fresh random generator.
 func getFreshRandomGenerator() *rand.Rand {
 	return rand.New(rand.NewSource(time.Now().UnixNano()))
 }
--- dicestrparser.go
+++ dicestrparser.go
@@ -7,11 +7,12 @@ import (
 	"strings"
 )
 
+// Max allow
+const maxRollArgSliceSize = 5
 const plus = "+"
 const minus = "-"
 
-// Parse command line Roll Arguments
-// Returns a map of DiceRoll and error
+// Parse command line Roll Arguments. Returns arrays of DiceRolls and errors.
 func ParseRollArgs(rollArgs []string) (diceRolls []DiceRoll, errors []error) {
 	for i := range rollArgs {
 		diceRoll, err := ParseRollArg(rollArgs[i])
@@ -24,17 +25,17 @@ func ParseRollArgs(rollArgs []string) (diceRolls []DiceRoll, errors []error) {
 	return diceRolls, errors
 }
 
-// Parse a single Roll Argument
-// Returns a DiceRoll
+// Parse and validates a roll arg. Returns a DiceRoll if valid, an error if invalid.
 func ParseRollArg(rollArg string) (diceRoll *DiceRoll, argErr error) {
 	// Validate arg format
 	rollArg = strings.ToLower(rollArg)
 	regExp := regexp.MustCompile("^[[:digit:]]+d[[:digit:]]+([+|-][[:digit:]]+)?$")
 	if !regExp.MatchString(rollArg) {
+		// Invalid roll arg, return error
 		return nil, createInvalidRollArgError(rollArg)
 	}
 
-	// Parse a roll argument and return it as a DiceRoll if valid
+	// Parse a roll argument and return it as a DiceRoll if valid. Return error if invalid
 	diceRoll = new(DiceRoll)
 	if rollArg, diceRoll.Modifier, argErr = evaluateModifier(rollArg); argErr != nil {
 		return nil, argErr
@@ -42,74 +43,92 @@ func ParseRollArg(rollArg string) (diceRoll *DiceRoll, argErr error) {
 	if diceRoll.DiceAmmount, diceRoll.DiceSize, argErr = evaluateDiceSizeAndAmmount(rollArg); argErr != nil {
 		return nil, argErr
 	}
-	return diceRoll, argErr
+	return diceRoll, nil
 }
 
+// Evaluates if a roll arg modifier is present.
+//   - If present, returns parseAndValidateModifier function call results.
+//   - If not present, returns the original roll arg, a zero value and no error.
 func evaluateModifier(rollArg string) (string, int, error) {
-	mod := 0
-	var modErr error = nil
+	// Detect if a modifier is present in roll arg. If present, parse and validate
 	if strings.ContainsAny(rollArg, plus) {
-		rollArg, mod, modErr = parseModifier(rollArg, plus)
+		return parseAndValidateModifier(rollArg, plus)
 
 	} else if strings.ContainsAny(rollArg, minus) {
-		rollArg, mod, modErr = parseModifier(rollArg, minus)
+		return parseAndValidateModifier(rollArg, minus)
 	}
-	return rollArg, mod, modErr
+	// Modifier not present in roll arg
+	return rollArg, 0, nil
 }
 
-func parseModifier(rollArg string, symbol string) (string, int, error) {
+// Parses and validate a roll arg modifier.
+//   - If valid, returns the roll arg string with the modifier part removed, the modifier value and nil value error.
+//   - If invalid, returns the original roll arg, a zero and an error.
+//   - For unexpected parsing errors, returns the original roll arg, a zero and an error.
+func parseAndValidateModifier(rollArg string, symbol string) (string, int, error) {
+	// Extract modifier value and validate its size
 	modSlices := strings.Split(rollArg, symbol)
-	if invalidErr := validateArgSize(modSlices[1]); invalidErr != nil {
+	if invalidErr := validateRollArgSliceSize(modSlices[1]); invalidErr != nil {
+		// Invalid modifier value
 		return rollArg, 0, invalidErr
 	}
-	mod, modErr := strconv.Atoi(modSlices[1])
-	if modErr == nil {
-		// Modifier is valid and processed, remove that slice from the rollArg
-		rollArg = modSlices[0]
+
+	// Parse a roll arg modifier and return its value if valid. Return zero and error if invalid
+	if modifier, modErr := strconv.Atoi(modSlices[1]); modErr == nil {
 		if strings.EqualFold(symbol, minus) {
-			mod = -mod
+			// Make the modifier value negative for minus modifier
+			modifier = -modifier
 		}
+		// Modifier is valid and processed, return rollArg without the modifier part
+		return modSlices[0], modifier, modErr
 	} else {
-		modErr = fmt.Errorf("error converting modifier %s: \n%s", modSlices[1], modErr.Error())
-		mod = 0
+		// Unexpected parsing error
+		return rollArg, 0, fmt.Errorf("error converting modifier %s: \n%s", modSlices[1], modErr.Error())
 	}
-	return rollArg, mod, modErr
 }
 
+// Validates, parses and return values of a valid roll arg. Returns an error for an invalid roll arg.
 func evaluateDiceSizeAndAmmount(rollArg string) (ammount int, size int, argErr error) {
+	// Slice the rollArg into slices and validate values
 	argSlices := strings.Split(rollArg, "d")
-	if ammount, argErr = parseDiceSlice(argSlices[0]); argErr != nil {
+	if ammount, argErr = parseRollArgSlice(argSlices[0]); argErr != nil {
 		return ammount, 0, argErr
 	} else if argErr = validateDiceAmmout(ammount); argErr != nil {
 		return ammount, 0, argErr
 	}
-	if size, argErr = parseDiceSlice(argSlices[1]); argErr != nil {
+	if size, argErr = parseRollArgSlice(argSlices[1]); argErr != nil {
 		return ammount, size, argErr
 	} else if argErr = validateDiceSize(size); argErr != nil {
 		return ammount, size, argErr
 	}
-	return ammount, size, argErr
+
+	// Returning valid values
+	return ammount, size, nil
 }
 
-func parseDiceSlice(diceSlice string) (int, error) {
-	if invalidErr := validateArgSize(diceSlice); invalidErr != nil {
+// Validates and parses a roll arg slice. Returns its value if valid, zero and an error if invalid.
+func parseRollArgSlice(argSlice string) (int, error) {
+	// Validate arg slice
+	if invalidErr := validateRollArgSliceSize(argSlice); invalidErr != nil {
 		return 0, invalidErr
 	}
-	dice, diceErr := strconv.Atoi(diceSlice)
-	if diceErr != nil {
-		diceErr = fmt.Errorf("error converting dice value of %s: \n%s", diceSlice, diceErr.Error())
-		dice = 0
+	// Parse and return arg slice value
+	if diceValue, diceErr := strconv.Atoi(argSlice); diceErr != nil {
+		return 0, fmt.Errorf("error converting dice value of %s: \n%s", argSlice, diceErr.Error())
+	} else {
+		return diceValue, diceErr
 	}
-	return dice, diceErr
 }
 
-func validateArgSize(arg string) error {
-	if len(arg) > 5 {
-		return fmt.Errorf("invalid value: %s. This is a dice roller, not a Pi calculator", arg)
+// Validates that arg slice is shorter than defined maxRollArgSliceSize to avoid overflow and long processing time.
+func validateRollArgSliceSize(rollArg string) error {
+	if len(rollArg) > maxRollArgSliceSize {
+		return fmt.Errorf("invalid value: %s. This is a dice roller, not a Pi calculator", rollArg)
 	}
 	return nil
 }
 
+// Returns a formatter error for invalid roll arg.
 func createInvalidRollArgError(rollArg string) error {
 	return fmt.Errorf("invalid roll arg: %s", rollArg)
 }
