60a7bd5d02a983d572b9c154485cfd3a0e1618b6
skyestalimit <skyestalimit@gmail.com>
2024-01-14 08:57:00 -0500

    clean up, drop high low support

---
+++ rollattributes.go
@@ -0,0 +1,83 @@
+package diceroller
+
+import "strings"
+
+type rollAttribute int
+
+const (
+	critAttrib         rollAttribute = iota + 1
+	halfAttrib         rollAttribute = iota + 1
+	advantageAttrib    rollAttribute = iota + 1
+	disadvantageAttrib rollAttribute = iota + 1
+	dropHighAttrib     rollAttribute = iota + 1
+	dropLowAttrib      rollAttribute = iota + 1
+)
+
+const (
+	critStr         string = "crit"
+	halfStr         string = "half"
+	advantageStr    string = "adv"
+	disadvantageStr string = "dis"
+	dropHighStr     string = "drophigh"
+	dropLowStr      string = "droplow"
+)
+
+var rollAttributeMap = map[rollAttribute]string{
+	critAttrib:         critStr,
+	halfAttrib:         halfStr,
+	advantageAttrib:    advantageStr,
+	disadvantageAttrib: disadvantageStr,
+	dropHighAttrib:     dropHighStr,
+	dropLowAttrib:      dropLowStr,
+}
+
+func rollAttributeMapKey(attribMap map[rollAttribute]string, wanted string) (rollAttribute, bool) {
+	for attrib, attribStr := range attribMap {
+		if strings.EqualFold(attribStr, wanted) {
+			return attrib, true
+		}
+	}
+	return 0, false
+}
+
+type attributes interface {
+	setRollAttrib(rollAttribute)
+	hasAttrib(rollAttribute) bool
+}
+
+type rollAttributes struct {
+	attribs map[rollAttribute]bool
+}
+
+func newRollAttributes() *rollAttributes {
+	newRollAttributes := new(rollAttributes)
+	newRollAttributes.attribs = make(map[rollAttribute]bool)
+	return newRollAttributes
+}
+
+func (rollAttribs rollAttributes) setRollAttrib(attrib rollAttribute) {
+	// Prevent attrib incompatibilities.
+	switch attrib {
+	case advantageAttrib:
+		delete(rollAttribs.attribs, disadvantageAttrib)
+		delete(rollAttribs.attribs, dropHighAttrib)
+		delete(rollAttribs.attribs, dropLowAttrib)
+	case disadvantageAttrib:
+		delete(rollAttribs.attribs, advantageAttrib)
+		delete(rollAttribs.attribs, dropHighAttrib)
+		delete(rollAttribs.attribs, dropLowAttrib)
+	case dropLowAttrib:
+		delete(rollAttribs.attribs, advantageAttrib)
+		delete(rollAttribs.attribs, disadvantageAttrib)
+		delete(rollAttribs.attribs, dropHighAttrib)
+	case dropHighAttrib:
+		delete(rollAttribs.attribs, advantageAttrib)
+		delete(rollAttribs.attribs, disadvantageAttrib)
+		delete(rollAttribs.attribs, dropLowAttrib)
+	}
+	rollAttribs.attribs[attrib] = true
+}
+
+func (dndAttrib rollAttributes) hasAttrib(attrib rollAttribute) bool {
+	return dndAttrib.attribs[attrib]
+}
